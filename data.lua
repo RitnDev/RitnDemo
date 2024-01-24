1a8004e2a6854a042680c463abe906f32b367ac9
RitnDev <ritnvideos@gmail.com>
2024-01-24 18:39:58 +0100

    0.0.1 - Création du mod démo

---
+++ data.lua
@@ -0,0 +1,9 @@
+if not ritnlib then 
+    require ("__RitnLib__.defines")
+end
+
+
+local RitnProtoRecipe = require(ritnlib.defines.class.prototype.recipe)
+
+
+RitnProtoRecipe("wooden-chest"):changeSubgroup("belt")
